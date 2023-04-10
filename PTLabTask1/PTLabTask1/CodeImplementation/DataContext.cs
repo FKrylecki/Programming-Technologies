04c49507a2ee7d798075f4d794b4d25e90107fe6
Filip Andrzejewski 242207 <242207@edu.p.lodz.pl>
2023-04-10 17:10:18 +0200

    Quick change - Data Lists from private to internal

--- PTLabTask1/PTLabTask1/CodeImplementation/DataContext.cs
+++ PTLabTask1/PTLabTask1/CodeImplementation/DataContext.cs
@@ -5,8 +5,8 @@ namespace Data.CodeImplementation
     internal class DataContext : IDataContext
     {
         internal List<IEvent> events = new();
-        List<IState> states = new();
-        List<IUser> users = new();
-        Dictionary<string, ICatalog> dictionary = new();
+        internal List<IState> states = new();
+        internal List<IUser> users = new();
+        internal Dictionary<string, ICatalog> dictionary = new();
     }
 }
