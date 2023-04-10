f728a0db508f10d9e90318bff382c816e3267079
FKrylecki <kryleckif@gmail.com>
2023-04-10 18:19:15 +0200

    Added Business Logic methods

--- PTLabTask1/Logic/API/IbusinessLogic.cs
+++ PTLabTask1/Logic/API/IbusinessLogic.cs
@@ -3,6 +3,7 @@
     public abstract class IbusinessLogic
     {
         public abstract void BuyItem(string userID, string stateID);
-
+        public abstract void SupplyItem(string userId, string stateId);
+        public abstract void ReturnItem(string userId, string stateId);
     }
 }
--- PTLabTask1/Logic/CodeImplementation/BusinessLogic.cs
+++ PTLabTask1/Logic/CodeImplementation/BusinessLogic.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Data.API;
 using Logic.API;
 
 namespace Logic.CodeImplementation
@@ -6,6 +6,14 @@ namespace Logic.CodeImplementation
     internal class BusinessLogic : IbusinessLogic
     {
         public override void BuyItem(string userID, string stateID)
+        {
+            throw new NotImplementedException();
+        }
+        public override void SupplyItem(string userID, string stateID)
+        {
+            throw new NotImplementedException();
+        }
+        public override void ReturnItem(string userID, string stateID)
         {
             throw new NotImplementedException();
         }
