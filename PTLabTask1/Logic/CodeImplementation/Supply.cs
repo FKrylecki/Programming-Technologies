077942fdcbf35950719929b0dda9bf69b832bff2
Filip Andrzejewski 242207 <242207@edu.p.lodz.pl>
2023-04-16 19:20:33 +0200

    Preset Data method generation (almost done) + Supply event

--- PTLabTask1/Logic/CodeImplementation/Supply.cs
+++ PTLabTask1/Logic/CodeImplementation/Supply.cs
@@ -1,6 +1,20 @@
-﻿namespace Logic.CodeImplementation
+﻿using Data.API;
+
+namespace Logic.CodeImplementation
 {
-    internal class Supply
+    internal class Supply : ISupply
     {
+        public string StateId { 
+            get; set;
+        }
+        public string UserId
+        {
+            get; set;
+        }
+
+        public Supply(string stateId, string userId) {
+            StateId = stateId;
+            UserId = userId;
+        }
     }
 }
