1a69c67d0ada015a90508811454ca2132666d28c
Filip Andrzejewski 242207 <95918062+filipandrzejewski@users.noreply.github.com>
2023-05-20 18:19:54 +0200

    Service Layer

---
+++ PTLabTask2/Services/API/IService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Data.API;
+
+
+public interface IService
+{
+    void AddCatalog(int id, string name, decimal price);
+    void RemoveCatalog(int id);
+    void UpdateCatalog(int id, string name, decimal price);
+    ICatalog GetCatalog(int id);
+    IEnumerable<ICatalog> GetCatalogsList();
+
+    //---------------------------------------------------
+    void AddUser(int id, string firstName, string lastName, string address);
+    void RemoveUser(int id);
+    void UpdateUser(int id, string firstName, string lastName, string address);
+    IUser GetUser(int id);
+    IEnumerable<IUser> GetUsersList();
+
+    //---------------------------------------------------
+    void AddState(int id, int quantity, int catalogId);
+    void RemoveState(int id);
+    IState GetState(int id);
+    IEnumerable<IState> GetStatesList();
+
+    //---------------------------------------------------
+    void AddEvent(int id, int stateId, int userId, int QuantityChanged);
+    void RemoveEvent(int id);
+    IEnumerable<IEvent> GetEventsList();
+
+    void SellItem(int id, int stateId, int userId, int QuantityChanged);
+    void ReturnItem(int id, int stateId, int userId, int QuantityChanged);
+    void SupplyItem(int id, int stateId, int userId, int QuantityChanged);
+}
---
+++ PTLabTask2/Services/CodeImplementation/Service.cs
@@ -0,0 +1,109 @@
+﻿
+using Data.API;
+using System.Collections.Generic;
+
+namespace Services.CodeImplementation
+{
+    internal class Service : IService
+    {
+        public void AddCatalog(int id, string name, decimal price)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddEvent(int id, int stateId, int userId, int QuantityChanged)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddState(int id, int quantity, int catalogId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddUser(int id, string firstName, string lastName, string address)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICatalog GetCatalog(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ICatalog> GetCatalogsList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IEvent> GetEventsList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IState GetState(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IState> GetStatesList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IUser GetUser(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IUser> GetUsersList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveCatalog(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveEvent(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveState(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveUser(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ReturnItem(int id, int stateId, int userId, int QuantityChanged)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SellItem(int id, int stateId, int userId, int QuantityChanged)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SupplyItem(int id, int stateId, int userId, int QuantityChanged)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCatalog(int id, string name, decimal price)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateUser(int id, string firstName, string lastName, string address)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
