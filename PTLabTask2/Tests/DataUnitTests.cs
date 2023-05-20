8b51455fc57543805ce921f4d7daf270ada73f19
FKrylecki <kryleckif@gmail.com>
2023-05-20 19:11:02 +0200

    Tests

---
+++ PTLabTask2/Tests/DataUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class DataUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
