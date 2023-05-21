be67b7d0ed2582583c953e38a2cd807311e2bbff
Filip Andrzejewski 242207 <95918062+filipandrzejewski@users.noreply.github.com>
2023-05-21 17:45:37 +0200

    User and Catalog to Model Data Templates

---
+++ PTLabsTask2/Presentation/Model/CodeImplementation/CatalogModelData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Presentation.Model;
+using Presentation.Model.API;
+
+namespace Presentation.Model.CodeImplementation
+{
+    internal class CatalogModelData : ICatalogModelData
+    {
+        public CatalogModelData(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public decimal Price
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ PTLabsTask2/Presentation/Model/CodeImplementation/UserModelData.cs
@@ -0,0 +1,30 @@
+﻿]using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Presentation.Model;
+using Presentation.Model.API;
+
+namespace Presentation.Model.CodeImplementation
+{
+    internal class UserModelData : IUserModelData
+    {
+        public UserModelData(int id, string firstName, string lastName, string address)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Id { get; set; }
+
+        public string Address { get; set; }
+    }
+}
+}
