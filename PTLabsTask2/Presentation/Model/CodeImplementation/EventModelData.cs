84abcd5f4d5dd135260c0c4000c731804b0383a2
FKrylecki <kryleckif@gmail.com>
2023-05-21 18:40:43 +0200

    Event and state model data

---
+++ PTLabsTask2/Presentation/Model/CodeImplementation/EventModelData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Model.CodeImplementation
+{
+    internal class EventModelData
+    {
+        public int Id
+        {
+            get;
+        }
+        public int StateId
+        {
+            get;
+        }
+        public int UserId
+        {
+            get;
+        }
+        public int QuantityChanged
+        {
+            get;
+            set;
+        }
+        public EventModelData(int id, int stateId, int userId, int quantityChanged) 
+        {
+            Id = id;
+            StateId = stateId;
+            UserId = userId;
+            QuantityChanged = quantityChanged;
+        }
+    }
+}
