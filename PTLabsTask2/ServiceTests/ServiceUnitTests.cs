888b070492cdbf540ace5c98db9b18f3783f70b9
Filip Andrzejewski 242207 <95918062+filipandrzejewski@users.noreply.github.com>
2023-05-28 14:40:07 +0200

    Service layer tests work

--- PTLabsTask2/ServiceTests/ServiceUnitTests.cs
+++ PTLabsTask2/ServiceTests/ServiceUnitTests.cs
@@ -2,8 +2,7 @@
 using Data.API;
 using Data.CodeImplementation;
 using Services.API;
-using Services.CodeImplementation;
-using System.Data.SqlTypes;
+
 
 namespace ServiceTests
 {
@@ -12,65 +11,59 @@ namespace ServiceTests
     {
        
         private const string defaultConnectionString = "Data Source = (localdb) Local;Initial Catalog = PTLabdb; Integrated Security = True";
-        private static IDataRepository dataRepository = new DataRepository(defaultConnectionString);
-        private Service s = new Service(dataRepository);
+        IService serviceRepo = IService.CreateNewService(defaultConnectionString);
 
         [TestMethod]
-        public void TestProductAsync()
+        public void CatalogTest()
         {
-            s.AddCatalog(7, "Potato", 222);
-            Task<IEnumerable<ICatalog>> a = s.GetCatalogsList();
-            Assert.IsNotNull(s.GetCatalogsList());
-            s.RemoveCatalog(7);
-            Assert.AreNotEqual(a, s.GetCatalogsList());
-            s.RemoveCatalog(7);
+            
+            serviceRepo.AddCatalog(1, "Sofa", 199);
+            Assert.IsNotNull(serviceRepo.GetCatalogsList());
+            Task<IEnumerable<ICatalog>> compare = serviceRepo.GetCatalogsList();
+            serviceRepo.RemoveCatalog(1);
+            Assert.AreNotEqual(compare, serviceRepo.GetCatalogsList());
+        }
+        [TestMethod]
+
+        public void UserTests()
+        {
+            serviceRepo.AddUser(1, "Filip", "Testt", "Testing");
+            Assert.IsNotNull(serviceRepo.GetUsersList());
+            Task<IEnumerable<IUser>> compare = serviceRepo.GetUsersList();
+            serviceRepo.RemoveUser(1);
+            Assert.AreNotEqual(compare, serviceRepo.GetUsersList());
         }
 
-        //[TestMethod]
-        //public void CatalogTest()
-        //{
-        //    IService serviceRepo = IService.CreateNewService(defaultConnectionString);
-        //    serviceRepo.AddCatalog(1, "Sofa", 199);
-        //    Assert.IsNotNull(serviceRepo.GetCatalogsList());
-        //    IEnumerable<ICatalog> compare = (IEnumerable<ICatalog>)serviceRepo.GetCatalogsList();
-        //    serviceRepo.RemoveCatalog(1);
-        //    Assert.AreNotEqual(compare, serviceRepo.GetCatalogsList());
-        //}
-        //[TestMethod]
+        [TestMethod]
+        public void EventsTest()
+        {
+            serviceRepo.AddUser(1, "Filip", "Test", "Test 1");
+            serviceRepo.AddCatalog(1, "Sofa", 199);
+            serviceRepo.AddState(1, 5, 1);
 
-        //public void UserTests()
-        //{
-        //    IService serviceRepo = IService.CreateNewService(defaultConnectionString);
-        //    serviceRepo.AddUser(1, "Filip", "Testt", "Testing");
-        //    Assert.IsNotNull(serviceRepo.GetUsersList());
-        //    IEnumerable<IUser> compare = (IEnumerable<IUser>)serviceRepo.GetUsersList();
-        //    serviceRepo.RemoveUser(1);
-        //    Assert.AreNotEqual(compare, serviceRepo.GetUsersList());
-        //}
+            Task<IEnumerable<IEvent>> compare = serviceRepo.GetEventsList();
 
-        //[TestMethod]
-        //public void EventsTest()
-        //{
-        //    IService serviceRepo = IService.CreateNewService(defaultConnectionString);
-        //    serviceRepo.AddUser(1, "Filip", "Test", "Test 1");
-        //    serviceRepo.AddCatalog(1, "Sofa", 199);
-        //    serviceRepo.AddState(1, 5, 1);
+            serviceRepo.SellItem(1, 1, 1, 3);
+            serviceRepo.ReturnItem(2, 1, 1, 2);
+            serviceRepo.SupplyItem(3, 1, 1, 1);
 
-        //    IEnumerable<IEvent> compare = (IEnumerable<IEvent>)serviceRepo.GetEventsList();
+            Assert.AreNotEqual(compare, serviceRepo.GetEventsList());
 
-        //    serviceRepo.SellItem(1, 1, 1, 3);
-        //    serviceRepo.ReturnItem(2, 1, 1, 2);
-        //    serviceRepo.SupplyItem(3, 1, 1, 1);
+            serviceRepo.RemoveEvent(1);
+            serviceRepo.RemoveEvent(2);
+            serviceRepo.RemoveEvent(3);
+            serviceRepo.RemoveState(1);
+            serviceRepo.RemoveCatalog(1);
+            serviceRepo.RemoveUser(1);
+        }
 
-        //    Assert.AreNotEqual(compare, serviceRepo.GetEventsList());
+        public void StateTests()
+        {
+            serviceRepo.AddCatalog(1, "Sofa", 199);
+            serviceRepo.AddState(1, 5, 1);
 
-        //    serviceRepo.RemoveEvent(1);
-        //    serviceRepo.RemoveEvent(2);
-        //    serviceRepo.RemoveEvent(3);
-        //    serviceRepo.RemoveState(1);
-        //    serviceRepo.RemoveCatalog(1);
-        //    serviceRepo.RemoveUser(1);
-        //}
+            Task<IEnumerable<IState>> compare = serviceRepo.GetStatesList();
+        }
 
     }
 }
